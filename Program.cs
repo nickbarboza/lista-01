ac361c90fe20e16bd07e02bfd3e4ee6e12827eae
Nicolly Barboza <nicolly_barboza@estudante.sesisenai.org.br>
2026-06-11 20:09:42 -0300

    q1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Informe a idade do visitante: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade >= 16)
+        {
+            Console.WriteLine("Entrada liberada.");
+        }
+        else
+        {
+            Console.WriteLine("Entrada permitida somente com responsável.");
+        } 
+        
+
+
