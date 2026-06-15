9b39dd74d69b7c4767afc57b904d2c2497a96587
Nicolly Barboza <nicolly_barboza@estudante.sesisenai.org.br>
2026-06-15 19:51:16 -0300

    q12

--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Informe o número final da contagem: ");
+﻿
+        Console.Write("Informe o número inicial da contagem regressiva: ");
         int numero = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= numero; i++)
+        for (int i = numero; i >= 0; i--)
         {
             Console.WriteLine(i);
         }
-    }
+
+        Console.WriteLine("Fim da contagem.");
+    
