7cbf2bb34591faed8502e0a28202439cab5bc5e5
Nicolly Barboza <nicolly_barboza@estudante.sesisenai.org.br>
2026-06-15 19:58:31 -0300

    q15

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿double soma = 0;
+﻿int positivos = 0;
+int negativos = 0;
+int zeros = 0;
 
-for (int i = 1; i <= 5; i++)
+for (int i = 1; i <= 10; i++)
 {
-    Console.Write("Informe um valor: ");
-    soma += double.Parse(Console.ReadLine());
+    Console.Write("Informe um número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > 0)
+        positivos++;
+    else if (numero < 0)
+        negativos++;
+    else
+        zeros++;
 }
 
-double media = soma / 5;
-
-Console.WriteLine("Soma dos valores: " + soma);
-Console.WriteLine("Média dos valores: " + media);
+Console.WriteLine("Quantidade de positivos: " + positivos);
+Console.WriteLine("Quantidade de negativos: " + negativos);
+Console.WriteLine("Quantidade de zeros: " + zeros);
